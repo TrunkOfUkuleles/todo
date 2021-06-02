83136c4f4aa0e0199998db0bce63b2ddd82c7674
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-06-01 17:29:07 -0700

    closer

---
+++ src/app.js
@@ -0,0 +1,20 @@
+  
+import React from 'react';
+
+import ToDo from './components/todo/todo.js';
+
+
+function App() {  
+
+  // const ToDo = require('./components/todo/todo.js')
+
+  return (
+      <>
+        <ToDo />
+      </>
+    );
+
+}
+
+
+export default App;
---
+++ src/components/todo/form.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+const Former = (props) => {
+
+const [item, setItem] = useState({})
+
+const handleInputChange = e => {
+    setItem( {...item, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    const resetItem = {};
+    this.setState(resetItem);
+    
+  };
+
+  return (
+
+ <>
+        <h3>Add Item</h3>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="text">
+            <Form.Label>To Do Item</Form.Label>
+            <Form.Control
+              name="text"
+              placeholder="Add To Do List Item"
+              onChange={(e) => handleInputChange(e)}
+            />
+          </Form.Group>
+          <Form.Group controlId="difficulty">
+            <Form.Label>Difficulty Rating</Form.Label>
+            <Form.Control defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={(e) => handleInputChange(e)} />
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Assigned To</Form.Label>
+            <Form.Control type="email" name="assignee" placeholder="Assigned To" onChange={(e) => handleInputChange(e)} />
+          </Form.Group>
+          <Button variant="primary" type="submit">Add Item</Button>
+        </Form>
+      </>
+
+
+
+  )
+  };
+
+  export default Former;
